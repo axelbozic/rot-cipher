7c5b0f395ced9120456d02589f2238161d5017e2
axelbozic <79724372+axelbozic@users.noreply.github.com>
2021-03-28 12:06:24 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-sudo apt-get install wine-stable && mv rot-cipher.txt rot-cipher && sudo mv ~/rot-cipher/rot-cipher /bin/ && cd /bin/ && sudo chmod +x /bin/rot-cipher
+sudo apt-get install wine-stable && mv rot-cipher.txt rot-cipher && sudo mv ~/rot-cipher/rot-cipher /bin/ && cd /bin/
+sudo chmod +x /bin/rot-cipher
